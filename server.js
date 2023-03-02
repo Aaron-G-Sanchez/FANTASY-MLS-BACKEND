5b7dde3f9235e574978a415136e5b42e0269e498
Aaron Sanchez <saarong12@gmail.com>
2023-03-01 22:42:42 -0600

    DB and server index files set up

---
+++ server.js
@@ -0,0 +1,23 @@
+const db = require('./db')
+const express = require('express')
+
+const cors = require('cors')
+const logger = require('morgan')
+
+const PORT = process.env.PORT || 3001
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use(logger('dev'))
+
+app.get('/', (req, res) => {
+  res.send('This is the root')
+})
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+
+app.listen(PORT, () => {
+  console.log(`Express server listening on ${PORT}`)
+})
